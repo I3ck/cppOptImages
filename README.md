8d0bde0047b1add2587b4d1e3f5be58d4bfcca7b
Martin Buck <buckmartin@buckmartin.de>
2015-04-02 17:14:53 +0200

    now generating all example images for xSquare

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cppOptImages
 Just hosting the example images for https://github.com/I3ck/cppOpt here
+In case you want to rebuild them, install `Gnuplot` ( http://www.gnuplot.info/ ) and `ImageMagick` ( http://www.imagemagick.org/ ) and run `generate_animations.sh`
--- generate_animations.sh
+++ generate_animations.sh
@@ -1,3 +1,26 @@
+echo generating xSquare/minimize.gif
 rm tmp/*
-gnuplot -e "xMin='-5'; xMax='5'; values='300'; inputFile='sourceFiles/xSquare/minimize.log'" animate_png.gpl
+gnuplot -e "xMin='-5'; xMax='5'; values='100'; inputFile='sourceFiles/xSquare/minimize.log'" animate_png.gpl
 convert -delay 1 tmp/*.png -loop 0 images/animations/xSquare/minimize.gif
+
+echo generating xSquare/maximize.gif
+rm tmp/*
+gnuplot -e "xMin='-5'; xMax='5'; values='100'; inputFile='sourceFiles/xSquare/maximize.log'" animate_png.gpl
+convert -delay 1 tmp/*.png -loop 0 images/animations/xSquare/maximize.gif
+
+echo generating xSquare/approach_3.gif
+rm tmp/*
+gnuplot -e "xMin='-5'; xMax='5'; values='100'; inputFile='sourceFiles/xSquare/approach_3.log'" animate_png.gpl
+convert -delay 1 tmp/*.png -loop 0 images/animations/xSquare/approach_3.gif
+
+echo generating xSquare/diverge_3.gif
+rm tmp/*
+gnuplot -e "xMin='-5'; xMax='5'; values='100'; inputFile='sourceFiles/xSquare/diverge_3.log'" animate_png.gpl
+convert -delay 1 tmp/*.png -loop 0 images/animations/xSquare/diverge_3.gif
+
+
+
+
+echo removing temporary files
+rm tmp/*
+
